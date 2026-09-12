85f50d19c9f69b88f147c092c2b909058b9df370
Vadim Vainshtein <vadim.vainshtein@devexperts.com>
2026-09-12 20:20:22 +0500

    Ignore IDE and build output directories

---
+++ src/main/java/kz/vainshtein/raftscore/RaftScoreApplication.java
@@ -0,0 +1,12 @@
+package kz.vainshtein.raftscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftScoreApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(RaftScoreApplication.class, args);
+    }
+}
---
+++ src/test/java/kz/vainshtein/raftscore/RaftScoreApplicationTests.java
@@ -0,0 +1,12 @@
+package kz.vainshtein.raftscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftScoreApplicationTests {
+
+    @Test
+    void applicationContextStarts() {
+    }
+}
